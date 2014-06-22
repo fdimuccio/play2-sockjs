e140a7afa8e287d827a225e80fbcf63f3f948d9d
Francesco Di Muccio <francesco.dimuccio@gmail.com>
2014-06-23 01:17:30 +0200

    Backported play.sockjs.core.FrameBuffer fix.

--- project/Build.scala
+++ project/Build.scala
@@ -2,7 +2,7 @@ import sbt._
 import sbt.Keys._
 
 object BuildSettings {
-  val buildVersion = "0.1.4"
+  val buildVersion = "0.1.5"
 
   val buildSettings = Defaults.defaultSettings ++ Seq(
     organization := "com.github.fdimuccio",
