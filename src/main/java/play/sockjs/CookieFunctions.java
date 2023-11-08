9261e2e0621c7583266c0341cfd1f7f6f549c84b
Matthias Kurz <m.kurz@irregular.at>
2023-11-08 11:26:03 +0100

    Adjust cookie api, it's Optional now

--- src/main/java/play/sockjs/CookieFunctions.java
+++ src/main/java/play/sockjs/CookieFunctions.java
@@ -10,8 +10,7 @@ public class CookieFunctions {
      * support jsessionid cookie
      */
     public static Function<Http.RequestHeader, Http.Cookie> jessionid = request -> {
-        Http.Cookie jsessionid = request.cookie("JSESSIONID");
-        String value = jsessionid != null ? jsessionid.value() : "dummy";
+        String value = request.cookie("JSESSIONID").map(c -> c.value()).orElse("dummy");
         return new Http.Cookie("JSESSIONID", value, null, "/", null, false, false, Http.Cookie.SameSite.STRICT);
     };
 
